c11cb38f17959a289c463db33971fdaacbb1c05b
alifaotys <alifaotysalsabilla@gmail.com>
2022-01-24 08:17:12 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <mete name="viewport" content="width-device-width, initial-scale=1.0">
+            <title>Alifa Oty Salsabilla | About</title>
+    </head>
+    <body>
+        <h1> Halaman About </h1>
+        <h3> <?php echo $nama; ?> </h3>
+        <p> <?php echo $email; ?> </p>
+        <img src="images/<?php echo $gambar; ?>" alt="mark lee" width="200px">
+    </body>    
+</html>    
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <mete name="viewport" content="width-device-width, initial-scale=1.0">
+            <title>Alifa Oty Salsabilla | Home</title>
+    </head>
+    <body>
+        <h1> Halaman Home </h1>
+
+    <script src="js/script.js"></script>
+        
+    </body>    
+</html>    
