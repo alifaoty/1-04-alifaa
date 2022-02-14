751d44fa921b1107b1a52b4b983eefeebfae5e76
alifaotys <alifaotysalsabilla@gmail.com>
2022-02-14 17:52:12 +0700

    fourth commit

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,17 +3,18 @@
 @section('container')
     <div class="title-top mb-5">
         <h2 class="fw-bold">Contact Us</h2>
-    <form>
+    <form method="post" action="{{route('contacts.store')}}">
+        {{csrf_field()}}
         <div class="form-floating mb-3">
-        <input type="text" class="form-control" id="inputname" placeholder="Nama">
+        <input type="text" class="form-control" id="inputname" placeholder="Nama" name="nama">
         <label for="inputname">Nama</label>
         </div>
         <div class="form-floating mb-3">
-        <input type="text" class="form-control" id="inputemail" placeholder="Email">
+        <input type="text" class="form-control" id="inputemail" placeholder="Email" name="email">
         <label for="inputemail">Email</label>
         </div>
         <div class="form-floating mb-3">
-        <textarea class="form-control" placeholder="Tinggalkan Pesan Disini" id="floatingTextArea" style="height: 100px"></textarea>
+        <textarea class="form-control" placeholder="Tinggalkan Pesan Disini" id="floatingTextArea" style="height: 100px" name="pesan"></textarea>
         <label for="floatingTextArea">Pesan</label>
         </div>
      <button type="submit" class="btn btn-primary">Submit</button>
