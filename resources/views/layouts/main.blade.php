a4e5c7485804a25497c84cd26a55b88d2e8459fb
alifaotys <alifaotysalsabilla@gmail.com>
2022-02-27 22:42:11 +0700

    sixth commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
     <title>Alifa Oty S | {{ $title }}</title>
   </head>
   <body>
-  
+
   @include('partials.navbar');
 
 <div class="container mt-4">
@@ -19,5 +19,6 @@
 </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
   </body>
 </html>
